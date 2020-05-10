9ed646b56421cdd0eea362314d54e5176ec39284
chicotobi <tobias310788@googlemail.com>
2020-05-10 23:17:40 +0200

    download clustered gpx

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import pandas as pd  
+import pandas as pd
 import plotly.graph_objects as go
 
 import flask
@@ -15,6 +15,12 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
 from sklearn.cluster import dbscan
+
+from flask.helpers import send_file
+
+import urllib
+import gpxpy
+import gpxpy.gpx
 
 token = "pk.eyJ1IjoiY2hpY290b2JpIiwiYSI6ImNrOXEzdXNzNDBnbnMzZnJ0NjdjNHJtenYifQ.1zM3EIf0UGBiauBZcEJAGg"
 p = "data.pickle"
@@ -37,9 +43,13 @@ b = list(map(str,a))
 m3 = dict(zip(a,b))
 
 app.layout = html.Div(
-    [dbc.Row([dbc.Col(
+    [dbc.Row(
+      [dbc.Col(html.H1("Geocache-Clustering algorithm with DBSCAN in RLP"), width="auto")],justify="center"
+              ),
+       dbc.Row(
+         [dbc.Col(
         html.Div([
-            html.Label("Epsilon in DBSCAN x 1e-5",htmlFor="eps"),
+            html.Label("Maximum neighbour distance", title="(Epsilon in DBSCAN * 1e-5)\nHigher value means more clusters",htmlFor="eps"),
             dcc.Slider(
                 id='eps',
                 min=0,
@@ -47,7 +57,7 @@ app.layout = html.Div(
                 value=14,
                 marks=m
             ),
-            html.Label("Min #Pts per cluster",htmlFor="npts"),
+            html.Label("Minimum points in Cluster",title="Higher values means less clusters",htmlFor="npts"),
             dcc.Slider(
                 id='npts',
                 min=0,
@@ -56,94 +66,166 @@ app.layout = html.Div(
                 marks=m2
             ),
             dcc.Checklist(
-                id="unclstrd_pts",
+                id="checkbox_plot_unclustered_points",
                     options=[
                         {'label': 'Show unclustered pts', 'value': 'show'},
                     ],
                     value=['show']
-            )  
+            ),
+            dcc.Dropdown(
+                    id='dropdown',
+                    options=[],
+                    value='all'
+            ),
+            html.A(
+                'Download Data',
+                id='download',
+                download="cluster.gpx",
+                href="",
+                target="_blank"
+            )
         ]),
         width=3
         ),
-    dbc.Col(    
+    dbc.Col(
         dcc.Graph(id='gra',animate=True)
     )]
     )
 ])
-   
-@app.callback(
-    Output('gra', 'figure'),
-    [Input('eps', 'value'),Input('npts','value'),
-     Input('unclstrd_pts','value')],
-    [State('gra','relayoutData')]
-    )
-def update_figure(eps,npts,unclstrd_pts,relayoutData):
-    # Clustering
-    fig = go.Figure()
-    
-    mdf = df.copy()    
+
+def get_layout_info(relayoutData):
     if relayoutData and "mapbox._derived" in relayoutData:
         mat = relayoutData["mapbox._derived"]["coordinates"]
-        
-        lon_min = min(np.array(mat)[:,0])
-        lon_max = max(np.array(mat)[:,0])
         lat_min = min(np.array(mat)[:,1])
         lat_max = max(np.array(mat)[:,1])
+        lon_min = min(np.array(mat)[:,0])
+        lon_max = max(np.array(mat)[:,0])
         lat0 = relayoutData["mapbox.center"]["lat"]
         lon0 = relayoutData["mapbox.center"]["lon"]
         zoom0 = relayoutData["mapbox.zoom"]
     else:
-        lon_min = 7.3
-        lon_max = 8.3
         lat_min = 49.2
         lat_max = 49.7
+        lon_min = 7.3
+        lon_max = 8.3
         lat0 = 49.436
         lon0 = 7.766
         zoom0 = 9.5
+    return lat_min, lat_max, lon_min, lon_max, lat0, lon0, zoom0
 
+def filter_data(relayoutData):
+    lat_min, lat_max, lon_min, lon_max, lat0, lon0, zoom0 = get_layout_info(relayoutData)
+    mdf = df.copy()
     mdf = mdf[mdf["Lat"]>lat_min]
     mdf = mdf[mdf["Lat"]<lat_max]
     mdf = mdf[mdf["Lon"]>lon_min]
     mdf = mdf[mdf["Lon"]<lon_max]
-    
-    np.random.seed(42) 
-    res = dbscan(mdf[["LatRad","LonRad"]].values,eps=eps*1e-5,min_samples=npts,metric='haversine')
-    grpd = mdf.copy()
-    grpd["cluster"] = res[1]
+    return mdf, lat0, lon0, zoom0
 
-    if 'show' in unclstrd_pts:
-        grp = grpd[grpd["cluster"]==-1]
-        fig.add_trace(go.Scattermapbox(
-            
-        mode = "markers",
-        marker=go.scattermapbox.Marker(
-                color='rgb(0, 0, 0)',
-                opacity=0.5
-            ),
-        lon = grp["Lon"],
-        lat=grp["Lat"],text="No cluster"))
+def get_dbscan_data(mdf,eps,npts):
+    np.random.seed(42)
+    res = dbscan(mdf[["LatRad","LonRad"]].values,eps=eps*1e-5,min_samples=npts,metric='haversine')
+    df3 = mdf.copy()
+    df3["cluster"] = res[1]
+    return df3
+
+def update_figure(d0, lat0, lon0, zoom0, plot_unclustered_points):
+    fig = go.Figure()
     
-    for idx_clstr in range(max(res[1])+1):
-        grp = grpd[grpd["cluster"]==idx_clstr]
-        fig.add_trace(go.Scattermapbox(
-        mode = "markers",
-        lon = grp["Lon"],
-        lat=grp["Lat"],
-        text="Cluster "+str(idx_clstr+1)))
+    if plot_unclustered_points:
+        d = d0[d0["cluster"]==-1]
+        fig.add_trace(
+            go.Scattermapbox(
+                mode = "markers",
+                marker = go.scattermapbox.Marker(
+                    color='rgb(0, 0, 0)',
+                    opacity=0.5),
+                lat = d["Lat"],
+                lon = d["Lon"],
+                text="No cluster"))
+
+    all_cluster_index = range(max(d0["cluster"])+1)
+    for cluster_index in all_cluster_index:
+        d = d0[d0["cluster"]==cluster_index]
+        fig.add_trace(
+            go.Scattermapbox(
+                mode = "markers",
+                lat = d["Lat"],
+                lon = d["Lon"],
+                text = "Cluster " + str(cluster_index)))
+        
     fig.update_layout(
-    go.Layout(
-        showlegend=False,   
-    mapbox = dict(
-            center=go.layout.mapbox.Center(lat=lat0,lon=lon0),
-            accesstoken=token,
-            zoom=zoom0
-            ),
-            title="Eps is " + str(eps) + " and npts is " + str(npts),
-            autosize=True,
-            hovermode='closest',
-            margin=dict(t=0, b=0, l=0, r=0)
-            ))
+        go.Layout(
+            showlegend=False,
+            mapbox = dict(
+                center=go.layout.mapbox.Center(lat=lat0,lon=lon0),
+                accesstoken=token,
+                zoom=zoom0),
+            autosize = True,
+            hovermode = 'closest',
+            margin = dict(t=0, b=0, l=0, r=0))) 
     return fig
 
+@app.callback(
+    [Output('gra', 'figure'),
+     Output("dropdown","options")],
+    [Input('eps', 'value'),
+     Input('npts','value'),
+     Input('checkbox_plot_unclustered_points','value')],
+    [State('gra','relayoutData')]
+    )
+def main_callback(eps,npts,checkbox_plot_unclustered_points,relayoutData):
+
+    plot_unclustered_points = ('show' in checkbox_plot_unclustered_points)
+    
+    mdf, lat0, lon0, zoom0 = filter_data(relayoutData)
+
+    d = get_dbscan_data(mdf,eps,npts)
+
+    fig = update_figure(d, lat0, lon0, zoom0, plot_unclustered_points)
+
+    # Update dropdown menu
+    all_cluster_index = range(max(d["cluster"])+1)
+    options = [{'label': i, 'value': i} for i in all_cluster_index]
+    
+    # Download file
+    txt = "hello world"
+    path = "tmp.txt"
+    with open(path, "w") as file:
+        file.write(txt)
+    send_file(path, as_attachment=True)
+    return fig, options
+
+@app.callback(
+    Output('download', 'href'),
+    [Input('eps', 'value'),
+     Input('npts','value'),
+     Input('dropdown', 'value')],
+    [State('gra','relayoutData')])
+def update_downloader(eps,npts,idx_cluster,relayoutData):
+    
+    mdf, lat0, lon0, zoom0 = filter_data(relayoutData)
+    
+    d = get_dbscan_data(mdf,eps,npts)
+    
+    tmp = d[d["cluster"]==idx_cluster]
+    
+    gpx = gpxpy.gpx.GPX()
+        
+    # Create first track in our GPX:
+    gpx_track = gpxpy.gpx.GPXTrack()
+    gpx.tracks.append(gpx_track)
+    
+    # Create first segment in our GPX track:
+    gpx_segment = gpxpy.gpx.GPXTrackSegment()
+    gpx_track.segments.append(gpx_segment)
+    
+    for index, row in tmp.iterrows():
+        lat = row["Lat"]
+        lon = row["Lon"]
+        gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(lat,lon))
+    gpxString = "data:text/csv;charset=utf-8," + urllib.parse.quote(gpx.to_xml())
+    return gpxString
+    
 if __name__ == '__main__':
-    app.server.run(debug=True,threaded=True)
+    app.server.run(debug=True)
