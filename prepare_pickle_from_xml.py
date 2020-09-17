4853d7e94dd28bf101ae36e2e06feac99efda9c2
chicotobi <tobias310788@googlemail.com>
2020-09-17 10:11:45 +0200

    subtract own geocaches, support direct zip extraction

--- prepare_pickle_from_xml.py
+++ prepare_pickle_from_xml.py
@@ -3,16 +3,32 @@
 
 import xml.etree.ElementTree as ET
 import glob
+import zipfile
 import pandas as pd  
 
-p = "/home/hofmann/geocache_clustering/geocache_clustering/data.pickle"
+p0 = "/home/hofmann/geocache_clustering/"
 
-all_markers = []
+# My own geocaches
+own = []
+zips = glob.glob(p0+'/own/*.zip')
+for z in zips:
+    zipfile.ZipFile(z, 'r').extractall(p0+"extracted_own/")  
+fs = glob.glob(p0+"extracted_own/*")
+for f in fs:
+    if not "wpts" in f:
+        root = ET.parse(f).getroot()
+        for i in range(7,len(root)):
+            name = root[i][1].text
+            own.append(name)
+            
+# All geocaches
 lats = []
 lons = []
 names = []
-fs = glob.glob('/home/hofmann/Downloads/geocache/*/*.gpx',recursive=True)
-
+zips = glob.glob(p0+'/geocache/*.zip')
+for z in zips:
+    zipfile.ZipFile(z, 'r').extractall(p0+"extracted/")
+fs = glob.glob(p0+"extracted/*")
 for f in fs:
     if not "wpts" in f:
         root = ET.parse(f).getroot()
@@ -20,12 +36,12 @@ for f in fs:
             lat = root[i].attrib['lat']
             lon = root[i].attrib['lon']
             name = root[i][1].text
-            lats.append(float(lat))
-            lons.append(float(lon))
-            names.append(name)
-
+            if name not in own:
+                lats.append(float(lat))
+                lons.append(float(lon))
+                names.append(name)
+            
 df = pd.DataFrame({"Name":names,"Lat":lats,"Lon":lons})
 df["LatRad"] = df["Lat"] / 180 * 3.141592653
 df["LonRad"] = df["Lon"] / 180 * 3.141592653
-df.to_pickle(p)
-
+df.to_pickle(p0 + "/data.pickle")
